3d8cccde7b4d6d10f0ec81596e35801d27820221
AhmedAmgadEid <ahmedamgad16746@gmail.com>
2024-10-27 22:35:46 +0300

    create signal and component model

---
+++ model/component.py
@@ -0,0 +1,6 @@
+
+class Component:
+    def __init__(self, amplitude, frequency, phase_shift):
+        self.frequency  = amplitude
+        self.amplitude = frequency
+        self.phase_shift = phase_shift
---
+++ model/signal.py
@@ -0,0 +1,11 @@
+
+class Signal:
+    def __init__(self):
+        self.x_data = []
+        self.y_data = []
+        self.components = []
+        self.max_frequency = None
+        self.min_frequenncy = None
+        
+        
+    
