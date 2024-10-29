06a6d6b2e1f537b01157197b60c45ae79bb4b014
Rawan Shoaib <rawanwalid978@gmail.com>
2024-10-29 23:16:16 +0200

    final freq

--- controller/frequency_plot_controller.py
+++ controller/frequency_plot_controller.py
@@ -5,23 +5,28 @@ class FrequencyPlotController:
     def __init__(self, main):
         self.main = main
         
-    def plot_frequency_domain(self, signal_data):
+    def plot_frequency_domain(self, y_signal_data):
         # Get data from the displayed signal
-        signal = np.array(signal_data)
-        sampling_rate = self.main.displayed_signal.sampling_freq 
+        signal = np.array(y_signal_data)
+        sampling_rate = 2 * 10
         number_of_samples = len(signal)
         
         # Calculate FFT
         fft_data = np.fft.fftshift(np.fft.fft(signal))
         X_mag = np.abs(fft_data)
-        freqs = np.fft.fftfreq(len(signal), 1/self.main.displayed_signal.sampling_freq)
+        freqs = np.fft.fftfreq(number_of_samples, 1/sampling_rate)
         # freqs = np.fft.fftshift(freqs)
+
+        # Keep only positive frequencies
+        positive_freq_indices = freqs >= 0
+        X_mag_positive = X_mag[positive_freq_indices]
+        freqs_positive = freqs[positive_freq_indices]
 
         # Clear previous plot
         self.main.frequency_domain_plot.clear()
         
         # Plot magnitude spectrum
-        self.main.frequency_domain_plot.plot(freqs, X_mag, pen='r')
+        self.main.frequency_domain_plot.plot(freqs_positive, X_mag_positive, pen='r')
         # self.main.frequency_domain_plot.plot(f[np.argmax(X_mag)], np.max(X_mag), symbol='x', symbolPen='y')
         
         # Set labels
